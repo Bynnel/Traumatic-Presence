01878827085018d633e327e44d5b0b1b303aa008
Lenny <187471588+Bynnel@users.noreply.github.com>
2024-12-23 22:00:54 +0200

    Changed Harmony ID

--- SharedProject/SharedSource/Plugin.cs
+++ SharedProject/SharedSource/Plugin.cs
@@ -1,46 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Diagnostics;
-using System.Drawing;
-using System.Net.Sockets;
-using System.Text;
-using Barotrauma;
-using Barotrauma.Networking;
+﻿using Barotrauma;
 using HarmonyLib;
 
-namespace TraumaticPresence
+namespace TraumaticPresence;
+
+public partial class Plugin : IAssemblyPlugin
 {
-    public partial class Plugin : IAssemblyPlugin
+    private static readonly Harmony harmony = new("lenny.barotrauma.traumaticpresence");
+
+    public void Initialize()
     {
-        static readonly Harmony harmony = new Harmony("lenny.barotrauma.discordrpc");
-        public void Initialize()
-        {
-            // When your plugin is loading, use this instead of the constructor
-            // Put any code here that does not rely on other plugins.
-            InitClient();
-            //RpcClient.Initialize();
-        }
+        InitClient();
+    }
 
-        public void OnLoadCompleted()
-        {
-            // After all plugins have loaded
-            // Put code that interacts with other plugins here.
-        }
+    public void OnLoadCompleted()
+    {
+    }
 
-        public void PreInitPatching()
-        {
-            harmony.PatchAll();
-        }
+    public void PreInitPatching()
+    {
+        harmony.PatchAll();
+    }
 
-        public void Dispose()
-        {
-            // Cleanup your plugin!
-            DebugConsole.NewMessage("Dispose() has been called.");
-            RpcClient.Dispose();
-            Timer.Dispose();
-            harmony.UnpatchAll("lenny.barotrauma.discordrpc");
-            //throw new NotImplementedException();
-        }
+    public void Dispose()
+    {
+        RpcClient.Dispose();
+        Timer.Dispose();
+        harmony.UnpatchAll("lenny.barotrauma.traumaticpresence");
     }
 }
